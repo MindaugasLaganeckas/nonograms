a3a0894c5aa67e761545bba4876497de76dea534
Mindaugas Laganeckas <mindaugas.laganeckas@3shape.com>
2020-05-12 16:43:06 +0200

    Working on file upload

--- com.nonograms.web/src/main/java/com/nonograms/web/applications/NonogramsApplication.java
+++ com.nonograms.web/src/main/java/com/nonograms/web/applications/NonogramsApplication.java
@@ -14,32 +14,30 @@
  * limitations under the License.
  */
 
-package com.nonograms.web.controllers;
+package com.nonograms.web.applications;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.nonograms.web.services.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
 @Controller
 @SpringBootApplication
 public class NonogramsApplication {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     SpringApplication.run(NonogramsApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner init(StorageService storageService) {
+    return (args) -> {
+      storageService.deleteAll();
+      storageService.init();
+    };
   }
 
   @RequestMapping("/")
