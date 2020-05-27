54b51154409e85f0f5ec2f7234ed0c81491e8145
Mindaugas Laganeckas <mindaugas.laganeckas@gmail.com>
2020-05-27 19:47:19 +0200

    Implement poor man's file upload example. WIP

---
+++ com.nonograms.web/src/main/java/com/example/uploadingfiles/FileUploadController.java
@@ -0,0 +1,84 @@
+package com.example.uploadingfiles;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+import com.google.common.io.Files;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.uploadingfiles.storage.StorageFileNotFoundException;
+import com.example.uploadingfiles.storage.StorageService;
+
+import com.nonograms.core.*;
+
+@Controller
+public class FileUploadController {
+
+	private final StorageService storageService;
+
+	@Autowired
+	public FileUploadController(StorageService storageService) {
+		this.storageService = storageService;
+	}
+
+	@GetMapping("/")
+	public String listUploadedFiles(Model model) throws IOException {
+
+		model.addAttribute("files", storageService.loadAll().map(
+				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+						"serveFile", path.getFileName().toString()).build().toUri().toString())
+				.collect(Collectors.toList()));
+
+		return "uploadForm";
+	}
+
+	@GetMapping("/files/{filename:.+}")
+	@ResponseBody
+	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+		Resource file = storageService.loadAsResource(filename);
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
+	}
+
+	@PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) throws IOException {
+		storageService.deleteAll();
+		storageService.init();
+		storageService.store(file);
+
+		final String fileName = file.getOriginalFilename();
+		final Path path = storageService.load(fileName);
+
+		final String fileExtension = Files.getFileExtension(fileName);
+		final String newName = path.toFile().getCanonicalPath().replace("." + fileExtension, "_gen." + fileExtension);
+		com.nonograms.core.Library.main(new String[]{path.toFile().getCanonicalPath(), newName, fileExtension});
+
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/";
+	}
+
+	@ExceptionHandler(StorageFileNotFoundException.class)
+	public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+		return ResponseEntity.notFound().build();
+	}
+
+}
